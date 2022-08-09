1fe52932ced48bb18f6a62f923940d06cdd60d77
Javul Mulla <javulmulla8@gmail.com>
2022-08-09 13:31:43 +0530

    javul: code arrangements

--- src/basiccorejavaprogram/FlipCoin.java
+++ src/basiccorejavaprogram/FlipCoin.java
@@ -13,9 +13,9 @@ public class FlipCoin {
 		System.out.println("Enter number ");
 		int toss = ab.nextInt(); // using nextInt() method
 		int Heads = 0, Tails = 0;
-		for (int i = 0; i < toss; i++) {
+		if (toss > 0) {
+			for (int i = 0; i < toss; i++) {
 			double flipcoin = Math.random();// using random method for random no
-			System.out.println(flipcoin);
 			if (flipcoin < 0.5) {
 				Heads++;
 				System.out.println("Heads");
@@ -26,7 +26,10 @@ public class FlipCoin {
 		}
 		int Head = (Heads * 100) / toss;
 		int Tail = (Tails * 100) / toss;
-		System.out.println("Percentage Head:" + Head);
-		System.out.println("Percentage Tail:" + Tail);
+		System.out.println("Percentage Head:" + Head +"%");
+		System.out.println("Percentage Tail:" + Tail +"%");
+	}
+	else
+		System.out.println("invalid num");
     }
 }
